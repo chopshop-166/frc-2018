7f731b53f00bc70aa77e5a86bff9874f43936314
Ben Beauregard <bot190@gmail.com>
2018-01-20 00:22:39 -0500

    Use spaces instead of tabs
    Replace all tabs with 4 spaces to match the style guide.

--- src/main/java/frc/team166/robot/OI.java
+++ src/main/java/frc/team166/robot/OI.java
@@ -12,48 +12,48 @@ import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj.buttons.JoystickButton;
 
 public class OI {
-	//creates joysticks (JoystickDrive is left, JoystickDrive2 is right)
-	public Joystick JoystickDrive;
-	public Joystick JoystickDrive2;
-	public XboxController Xboxtempest;
-	public JoystickButton XboxAbutton;
-	public JoystickButton XboxXButton;
+    //creates joysticks (JoystickDrive is left, JoystickDrive2 is right)
+    public Joystick JoystickDrive;
+    public Joystick JoystickDrive2;
+    public XboxController Xboxtempest;
+    public JoystickButton XboxAbutton;
+    public JoystickButton XboxXButton;
 
-	public OI() {
-		//defines the joysticks as joysticks and assigns left and right
-		JoystickDrive = new Joystick(RobotMap.Controller.leftcontrol);
-		JoystickDrive2 = new Joystick(RobotMap.Controller.rightcontrol);
-		Xboxtempest = new XboxController(RobotMap.Controller.Xboxcontrol);
-		XboxAbutton = new JoystickButton(Xboxtempest, RobotMap.Buttons.XboxAbutton);
-		XboxAbutton.whileHeld(Robot.drive.Ebrake());
-		XboxXButton = new JoystickButton(Xboxtempest, RobotMap.Buttons.XboxXbutton);
-		XboxXButton.whileHeld(Robot.drive.DriveStraight());
-	}
-	//// CREATING BUTTONS
-	// One type of button is a joystick button which is any button on a
-	//// joystick.
-	// You create one by telling it which joystick it's on and which button
-	// number it is.
-	// Joystick stick = new Joystick(port);
-	// Button button = new JoystickButton(stick, buttonNumber);
+    public OI() {
+        //defines the joysticks as joysticks and assigns left and right
+        JoystickDrive = new Joystick(RobotMap.Controller.leftcontrol);
+        JoystickDrive2 = new Joystick(RobotMap.Controller.rightcontrol);
+        Xboxtempest = new XboxController(RobotMap.Controller.Xboxcontrol);
+        XboxAbutton = new JoystickButton(Xboxtempest, RobotMap.Buttons.XboxAbutton);
+        XboxAbutton.whileHeld(Robot.drive.Ebrake());
+        XboxXButton = new JoystickButton(Xboxtempest, RobotMap.Buttons.XboxXbutton);
+        XboxXButton.whileHeld(Robot.drive.DriveStraight());
+    }
+    //// CREATING BUTTONS
+    // One type of button is a joystick button which is any button on a
+    //// joystick.
+    // You create one by telling it which joystick it's on and which button
+    // number it is.
+    // Joystick stick = new Joystick(port);
+    // Button button = new JoystickButton(stick, buttonNumber);
 
-	// There are a few additional built in buttons you can use. Additionally,
-	// by subclassing Button you can create custom triggers and bind those to
-	// commands the same as any other Button.
+    // There are a few additional built in buttons you can use. Additionally,
+    // by subclassing Button you can create custom triggers and bind those to
+    // commands the same as any other Button.
 
-	//// TRIGGERING COMMANDS WITH BUTTONS
-	// Once you have a button, it's trivial to bind it to a button in one of
-	// three ways:
+    //// TRIGGERING COMMANDS WITH BUTTONS
+    // Once you have a button, it's trivial to bind it to a button in one of
+    // three ways:
 
-	// Start the command when the button is pressed and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenPressed(new ExampleCommand());
+    // Start the command when the button is pressed and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenPressed(new ExampleCommand());
 
-	// Run the command while the button is being held down and interrupt it once
-	// the button is released.
-	// button.whileHeld(new ExampleCommand());
+    // Run the command while the button is being held down and interrupt it once
+    // the button is released.
+    // button.whileHeld(new ExampleCommand());
 
-	// Start the command when the button is released and let it run the command
-	// until it is finished as determined by it's isFinished method.
-	// button.whenReleased(new ExampleCommand());
+    // Start the command when the button is released and let it run the command
+    // until it is finished as determined by it's isFinished method.
+    // button.whenReleased(new ExampleCommand());
 }
--- src/main/java/frc/team166/robot/subsystems/Cart.java
+++ src/main/java/frc/team166/robot/subsystems/Cart.java
@@ -17,67 +17,67 @@ import frc.team166.robot.commands.SubsystemCommand;
  * An example subsystem.  You can replace me with your own Subsystem.
  */
 public class Cart extends Subsystem {
-	Victor motor = new Victor(1);
-	DigitalInput sensorright = new DigitalInput(8);
-	DigitalInput sensorleft = new DigitalInput(9);
+    Victor motor = new Victor(1);
+    DigitalInput sensorright = new DigitalInput(8);
+    DigitalInput sensorleft = new DigitalInput(9);
 
-	private boolean sensorrighton() {
-		if (sensorright.get())
-			return true;
-		else
-			return false;
-	}
+    private boolean sensorrighton() {
+        if (sensorright.get())
+            return true;
+        else
+            return false;
+    }
 
-	private boolean sensorlefton() {
-		if (sensorleft.get())
-			return true;
-		else
-			return false;
-	}
+    private boolean sensorlefton() {
+        if (sensorleft.get())
+            return true;
+        else
+            return false;
+    }
 
-	private void setspeed(double speed) {
-		motor.set(speed);
-	}
+    private void setspeed(double speed) {
+        motor.set(speed);
+    }
 
-	public void initDefaultCommand() {
+    public void initDefaultCommand() {
 
-	}
+    }
 
-	public Command moveleft() {
-		return new SubsystemCommand(this) {
-			@Override
-			protected void initialize() {
-				setspeed(0.2);
-			}
+    public Command moveleft() {
+        return new SubsystemCommand(this) {
+            @Override
+            protected void initialize() {
+                setspeed(0.2);
+            }
 
-			@Override
-			protected boolean isFinished() {
-				return sensorlefton();
-			}
+            @Override
+            protected boolean isFinished() {
+                return sensorlefton();
+            }
 
-			@Override
-			protected void end() {
-				setspeed(0);
-			}
-		};
-	}
+            @Override
+            protected void end() {
+                setspeed(0);
+            }
+        };
+    }
 
-	public Command moveright() {
-		return new SubsystemCommand(this) {
-			@Override
-			protected void initialize() {
-				setspeed(-0.2);
-			}
+    public Command moveright() {
+        return new SubsystemCommand(this) {
+            @Override
+            protected void initialize() {
+                setspeed(-0.2);
+            }
 
-			@Override
-			protected boolean isFinished() {
-				return sensorrighton();
-			}
+            @Override
+            protected boolean isFinished() {
+                return sensorrighton();
+            }
 
-			@Override
-			protected void end() {
-				setspeed(0);
-			}
-		};
-	}
+            @Override
+            protected void end() {
+                setspeed(0);
+            }
+        };
+    }
 }
