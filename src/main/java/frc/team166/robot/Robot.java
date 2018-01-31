ceaecc016ffa4cc6dd4d5ad6da4fee2fba12e33d
Ben Beauregard <bot190@gmail.com>
2018-01-30 20:09:51 -0500

    Remove cart subsystem
    The cart subsystem now lives in its own branch and not master.

--- src/main/java/frc/team166/robot/Robot.java
+++ src/main/java/frc/team166/robot/Robot.java
@@ -28,7 +28,7 @@ public class Robot extends TimedRobot {
     public static final ExampleSubsystem kExampleSubsystem = new ExampleSubsystem();
     //this just adds a drive subsystem
     public static final Drive drive = new Drive();
-    public static final Manipulator manip = new Manipulator();
+    public static final Manipulator manipulator = new Manipulator();
     public static OI m_oi;
 
     Command m_autonomousCommand;
