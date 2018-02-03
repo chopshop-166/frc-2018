52d246a112b3c62652ca5046aaca316c5619a7d9
Ben Beauregard <bot190@gmail.com>
2018-02-03 10:40:18 -0500

    Remove subsystems that won't run on the cart.

--- src/main/java/frc/team166/robot/OI.java
+++ src/main/java/frc/team166/robot/OI.java
@@ -7,20 +7,9 @@
 
 package frc.team166.robot;
 
-import frc.team166.chopshoplib.controls.ButtonJoystick;
-import frc.team166.chopshoplib.controls.ButtonXboxController;;
-
 public class OI {
-    // Creates joysticks
-    public ButtonJoystick leftDriveStick;
-    public ButtonJoystick rightDriveStick;
-    public ButtonXboxController xBoxTempest;
 
     public OI() {
-        //defines the joysticks as joysticks and assigns left and right
-        leftDriveStick = new ButtonJoystick(RobotMap.Controller.leftcontrol);
-        rightDriveStick = new ButtonJoystick(RobotMap.Controller.rightcontrol);
-        xBoxTempest = new ButtonXboxController(RobotMap.Controller.Xboxcontrol);
     }
     //// CREATING BUTTONS
     // One type of button is a joystick button which is any button on a
--- src/main/java/frc/team166/robot/RobotMap.java
+++ src/main/java/frc/team166/robot/RobotMap.java
@@ -14,47 +14,5 @@ package frc.team166.robot;
  * floating around.
  */
 public class RobotMap {
-    public static class CAN {
-        //changes motor ports into integers
-        public final static int FRONT_RIGHT = 1;
-        public final static int FRONT_LEFT = 3;
-        public final static int BACK_RIGHT = 4;
-        public final static int BACK_LEFT = 2;
-        public final static int ROLLER_LEFT = 5;
-        public final static int ROLLER_RIGHT = 6;
-    }
 
-    public static class Controller {
-        //changes controller ports into integers
-        public final static int leftcontrol = 0;
-        public final static int rightcontrol = 1;
-        public final static int Xboxcontrol = 2;
-    }
-
-    public static class AnalogInputs {
-        //changes input ports into integers
-        public final static int tempestgyro = 1;
-        public final static int IR = 2;
-    }
-
-    public static class Buttons {
-        //changes button ports into integers
-        public final static int XboxAbutton = 1;
-        public final static int XboxXbutton = 3;
-    }
-
-    public static class Encoders {
-    }
-
-    public static class Solenoids {
-        public static final int MANIPULATOR_SOLENOID_A = 1;
-        public static final int MANIPULATOR_SOLENOID_B = 2;
-    }
-
-    public static class Preferences {
-        public static final String MANIPULATOR_MOTOR_INTAKE_SPEED = "manipulatorMotorIntakeSpeed";
-        public static final String MANIPULATOR_MOTOR_DISCHARGE_SPEED = "manipulatorMotorDischargeSpeed";
-        public static final String CUBE_PICKUP_DISTANCE = "cubePickupDistance";
-        public static final String CUBE_EJECT_WAIT_TIME = "cubeEjectWaitTime";
-    }
 }
